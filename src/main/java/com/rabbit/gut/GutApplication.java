bf9a1bf79d7fd34b745bcc873064c661c6a4ab59
agutnik76 <agutnik@gmail.com>
2018-08-28 14:49:02 +0300

    1

---
+++ src/main/java/com/rabbit/gut/GutApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.gut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GutApplication.class, args);
+    }
+}
